dd70192f853e53ecc5fc1a9bbdc5b5d10ba518a4
Apricot <50947830+Apricot-ale@users.noreply.github.com>
2025-03-07 17:02:06 +0900

    Box: Add ace compat

---
+++ mavic_box/ace/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+	class Mavic_Box_ace
+	{
+		name = "Mavic 3 (Improved) - Box - ace compat";
+		author = "$STR_Mavic_Author";
+		url = "https://steamcommunity.com/sharedfiles/filedetails/?id=3401580668";
+		requiredVersion = 2.18;
+		requiredAddons[] = {"Mavic_Box","ace_logistics_uavbattery"};
+		skipWhenMissingDependencies = 1;
+		units[] = {"Mavic_Box_AIO_Mavic3A_Items_F","Mavic_Box_NATO_Mavic3E_Items_F","Mavic_Box_NATO_Mavic3T_Items_F","Mavic_Box_CSAT_Mavic3E_Items_F","Mavic_Box_CSAT_Mavic3T_Items_F","Mavic_Box_AAF_Mavic3E_Items_F","Mavic_Box_AAF_Mavic3T_Items_F","Mavic_Box_IDAP_Mavic3E_Items_F","Mavic_Box_IDAP_Mavic3T_Items_F","Mavic_Box_GEN_Mavic3E_Items_F","Mavic_Box_GEN_Mavic3T_Items_F","Mavic_Box_EAF_Mavic3E_Items_F","Mavic_Box_EAF_Mavic3T_Items_F"};
+		weapons[] = {};
+	};
+};
+
+#include "CfgVehicles.hpp"
