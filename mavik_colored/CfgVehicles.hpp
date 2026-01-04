ab1c4b564b87c6566d2e73e2a9c27fe5c0331618
Apricot <50947830+Apricot-ale@users.noreply.github.com>
2026-01-05 05:11:29 +0900

    Add identification marks submod
    from https://steamcommunity.com/sharedfiles/filedetails/?id=3588424440

---
+++ mavik_colored/CfgVehicles.hpp
@@ -0,0 +1,71 @@
+class CfgVehicles
+{
+	class Mavic3_drone_base_F;
+	class Mavic3T_drone_base_F;
+	class Mavic3N_drone_base_F;
+	class Mavic3X_drone_base_F;
+	class Mavic_3_OPF: Mavic3_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_opfor_CO.paa"};
+	};
+	class Mavic_3_BLU: Mavic3_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_blufor_CO.paa"};
+	};
+	class Mavic_3_IND: Mavic3_drone_base_F
+	{
+	};
+	class Mavic_3_CIV: Mavic3_drone_base_F
+	{
+	};
+	class Mavic_3T_OPF: Mavic3T_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_opfor_CO.paa"};
+	};
+	class Mavic_3T_BLU: Mavic3T_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_blufor_CO.paa"};
+	};
+	class Mavic_3T_IND: Mavic3T_drone_base_F
+	{
+	};
+	class Mavic_3T_CIV: Mavic3T_drone_base_F
+	{
+	};
+	class Mavic_3N_OPF: Mavic3N_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_opfor_CO.paa"};
+	};
+	class Mavic_3N_BLU: Mavic3N_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_blufor_CO.paa"};
+	};
+	class Mavic_3N_IND: Mavic3N_drone_base_F
+	{
+	};
+	class Mavic_3N_CIV: Mavic3N_drone_base_F
+	{
+	};
+	class Mavic_3X_OPF: Mavic3X_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_opfor_CO.paa"};
+	};
+	class Mavic_3X_BLU: Mavic3X_drone_base_F
+	{
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"mavik_colored\textures\body_blufor_CO.paa"};
+	};
+	class Mavic_3X_IND: Mavic3X_drone_base_F
+	{
+	};
+	class Mavic_3X_CIV: Mavic3X_drone_base_F
+	{
+	};
+};
---
+++ mavik_colored/config.cpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+	class Mavic_Colored
+	{
+		name = "Mavic 3 (Improved) - Colored";
+		author = "$STR_Mavic_Author";
+		url = "https://steamcommunity.com/sharedfiles/filedetails/?id=3401580668";
+		requiredVersion = 2.18;
+		requiredAddons[] = {"A3_Data_F_Decade_Loadorder","Mavic_Core"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+#include "CfgVehicles.hpp"
